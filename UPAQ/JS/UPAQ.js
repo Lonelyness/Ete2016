4e6df24a71a27b433fc78bf7dd05950793091666
NAZAIRE Aude <audeladesmers67@gmail.com>
2016-05-18 12:35:56 -0400

    Mise en Forme

--- UPAQ/JS/UPAQ.js
+++ UPAQ/JS/UPAQ.js
@@ -5,7 +5,6 @@ var width = document.documentElement.clientWidth*0.8,
 var public_spreadsheet_url = 'https://docs.google.com/spreadsheets/d/16hcaoiUAwm_ANY3G-mjkV2dherVcaqGcoDyhL0oeFtQ/pubhtml';
 
 function drawChart(data) {
-	console.log(data);
 	d3.select("body").selectAll('div')
 		.data(data)
 		.enter()
@@ -13,10 +12,9 @@ function drawChart(data) {
 		.attr('id',function(d) {
 			return d.id;
 		})
-		.style('width', width + "px")
 		.style('height', "80px")
 		.html(function(d) {
-			return '<p class="flotte"><img src='+d.image+' style="width:60px;"> </p><p> <br />' + d.Nom + '<br /> <i>' + d.Fonction + '</i> </p>' ;
+			return '<p class="flotte"><img src="' + d.image + '" style="width:60px;" > </p><p> <br /> <b>' + d.Nom + '</b> <br /> <i>' + d.Fonction + '</i><br />'+ d.Explication +'</p>' ;
 		})
 };
 
