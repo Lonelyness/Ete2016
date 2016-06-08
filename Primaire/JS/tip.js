613d2cb0dfb15b4962c73fc62ca51318ac792e95
NAZAIRE Aude <audeladesmers67@gmail.com>
2016-06-08 17:11:54 -0400

    Tooltip Fixe

--- Primaire/JS/tip.js
+++ Primaire/JS/tip.js
@@ -41,8 +41,8 @@ d3.tip = function() {
     while(i--) nodel.classed(directions[i], false)
     coords = direction_callbacks.get(dir).apply(this)
     nodel.classed(dir, true).style({
-      top: (coords.top +  poffset[0]) + 'px',
-      left: (coords.left + poffset[1]) + 'px'
+      top: (100 +  poffset[0]) + 'px',
+      left: (150 + poffset[1]) + 'px'
     })
 
     return tip
