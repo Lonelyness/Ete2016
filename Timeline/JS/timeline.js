9393505123a1c36e965c09e55db505976a929f12
NAZAIRE Aude <audeladesmers67@gmail.com>
2016-06-16 18:33:30 -0400

    Projet Attentats : Version 1

--- Timeline/JS/timeline.js
+++ Timeline/JS/timeline.js
@@ -1,5 +1,5 @@
 ﻿var format = d3.time.format("%d/%m/%Y");
-var startDate = format.parse("01/01/1945");
+var startDate = format.parse("01/01/1948");
 var today = new Date();
 var dd = today.getDate();
 var mm = today.getMonth()+1; //January is 0!
@@ -7,9 +7,13 @@ var yyyy = today.getFullYear();
 var endDate = format.parse(dd+"/"+mm+"/"+yyyy);
 
 
-var margin = {top: 200, right: 100, bottom: 70, left: 70},
+document.getElementById('titre').style.width = Math.min(620,window.innerWidth)+"px";
+
+
+if (window.innerWidth>500) {
+var margin = {top: 220, right: 100, bottom: 70, left: 90},
     width = Math.min(620,window.innerWidth) - margin.left - margin.right,
-    height = 600 - margin.top - margin.bottom;
+    height = 280 - margin.top - margin.bottom;
 
 var x = d3.time.scale()
     .domain([startDate, endDate])
@@ -50,8 +54,8 @@ function drawChart(data) {
 			.attr("class", "separation")
 			.attr("x1", x(startDate))
 			.attr("x2", x(endDate))
-			.attr("y1", 30)
-			.attr("y2", 30)
+			.attr("y1", -20)
+			.attr("y2", -20)
 			.attr("opacity", .6)
 			.attr("stroke", "black");
 	
@@ -59,10 +63,9 @@ function drawChart(data) {
 		.data(data)
 		.enter()
 		.append('circle')
-		.attr("cy",30)
+		.attr("cy",-20)
 		.attr("cx",function(d) {
 			var date = d.date_assassinat;
-			//var y = d.date_assassinat.slice(-4);
 			var y = x(format.parse(date));
 			
 			if (d.nom=="Martin Luther King") {
@@ -84,7 +87,7 @@ function drawChart(data) {
 		.enter()
 		.append('text')
 		.attr("class","nomtrav")
-		.attr("y",50)
+		.attr("y",-30)
 		.attr("x",function(d) {
 			var date = d.date_assassinat;
 			var y = x(format.parse(date));
@@ -103,7 +106,7 @@ function drawChart(data) {
 		.attr("transform",function(d) {
 			var date = d.date_assassinat;
 			var y = x(format.parse(date));
-		return "rotate(55 "+ y +" 50)"
+		return "rotate(-55 "+ y +" -30)"
 		});
 		
 	
@@ -122,4 +125,103 @@ function draw(data, tabletop) {
 
 renderSpreadsheetData();
  svg.call(tip);
+}
 
+
+if (window.innerWidth<=500) {
+	var margin = {top: 30, right: 30, bottom: 30, left: 55},
+    width = window.innerWidth - margin.left - margin.right,
+    height = 600 - margin.top - margin.bottom;
+
+var x = d3.time.scale()
+    .domain([startDate, endDate])
+    .nice(d3.time.month)
+    .range([height, 0]);
+
+var svg = d3.select("body").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+svg.append("g")
+    .attr("class", "x axis")
+    .call(d3.svg.axis().scale(x).orient("left"));
+	
+//lien de la google sheet
+var public_spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1SjNM3P5I7u-xvYz4wbVEtswydhte83poBOI-aYkprTg/pubhtml'	
+function drawChart(data) {
+	
+	var group = svg.append("g");
+	
+	group.append("line")
+			.attr("class", "separation")
+			.attr("y1", x(startDate))
+			.attr("y2", x(endDate))
+			.attr("x1", 30)
+			.attr("x2", 30)
+			.attr("opacity", .6)
+			.attr("stroke", "black");
+	
+	group.selectAll('cicle')
+		.data(data)
+		.enter()
+		.append('circle')
+		.attr("cx",30)
+		.attr("cy",function(d) {
+			var date = d.date_assassinat;
+			//var y = d.date_assassinat.slice(-4);
+			var y = x(format.parse(date));		
+			if (d.nom=="Martin Luther King") {
+				y +=6;
+				}
+			if (d.nom=="Robert F. Kennedy") {
+				y -=6;
+				}
+			return y;
+		})
+		.attr("r",7)
+		.style("stroke","black")
+		.style("fill","white");
+		
+	group.selectAll('text')
+		.data(data)
+		.enter()
+		.append('text')
+		.attr("class","nomtrav")
+		.attr("x",50)
+		.attr("y",function(d) {
+			var date = d.date_assassinat;
+			var y = x(format.parse(date));
+			if (d.nom=="Martin Luther King") {
+				y +=7;
+				}
+			if (d.nom=="Robert F. Kennedy") {
+				y -=7;
+				}
+			
+			return y+7;
+		})
+		.style("fill","black")
+		.text(function(d) {
+			return d.nom;
+		});
+		
+	
+			
+}	
+//Appel à la Google sheet par 2 fonctions
+ function renderSpreadsheetData() {
+    Tabletop.init( { key: public_spreadsheet_url,
+                     callback: draw,
+                     simpleSheet: true } )
+}
+function draw(data, tabletop) {
+  // draw chart
+  drawChart(data);
+	}
+
+renderSpreadsheetData();
+	
+	
+}
