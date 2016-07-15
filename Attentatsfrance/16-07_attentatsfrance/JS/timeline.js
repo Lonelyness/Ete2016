05d3a254427af784d152e1ea6f6eb77c737f4013
Aude Nazaire <audeladesmers67@gmail.com>
2016-07-15 17:34:19 -0400

    Projet 15a 15b et 17

---
+++ Attentatsfrance/16-07_attentatsfrance/JS/timeline.js
@@ -0,0 +1,152 @@
+﻿//lien de la google sheet
+var public_spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1cQVOLeFr4rTnNck4qMr-phxeNx2CTmhqeF6-Odz5V1o/pubhtml?gid=788345292&single=true'
+// Si tooltip fixe et affichage du premier élement fixe=true;
+var fixe = true;
+
+var format = d3.time.format("%d/%m/%Y");
+var startDate = format.parse("01/01/2015");
+var today = new Date();
+var dd = today.getDate();
+var mm = today.getMonth()+1; //January is 0!
+var yyyy = today.getFullYear();
+var endDate = format.parse(dd+"/"+mm+"/"+yyyy);
+var formatDate = d3.time.format("%m/%Y");
+
+var decalage = 300;
+
+if (window.innerWidth<800) {
+	decalage = 260
+};
+if (window.innerWidth<620) {
+	decalage = 220;
+};
+if (window.innerWidth<375) {
+	decalage = 120;
+};
+
+
+
+
+
+document.getElementById('titre').style.width = Math.min(590,window.innerWidth-30)+"px";
+
+
+var margin = {top: 20, right: 95, bottom: 20, left: 70},
+    width = Math.min(window.innerHeight,600) - margin.left - margin.right,
+    height = 600 - margin.top - margin.bottom;
+
+var x = d3.time.scale()
+    .domain([startDate, endDate])
+    .nice(d3.time.month)
+    .range([height, 0]);
+
+var svg = d3.select("body").append("svg")
+	.attr("width", width + margin.left + margin.right)
+	.attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+svg.append("g")
+    .attr("class", "x axis")
+    .call(d3.svg.axis().scale(x).orient("left").tickFormat(formatDate));
+	
+var tip = d3.tip()
+		.attr('class', 'd3-tip')
+		.offset(function(d) {
+			if ((fixe==false)&&(window.innerWidth<420)&&(d.uid==12)) {
+				return [200,decalage];
+				}
+			return [80,decalage];
+		})
+		.html(function(d,i) {
+			var ville = d.ville;
+			var etat = d.etat;
+			var nom = d.nom;
+			var date = d.Date;
+			var age = d.age;
+			var note = d.detail;
+			return '<div class=texte><b>Le '+ date + ' </b><br/><span class="note">' + note + '</span>' ;
+		;});	
+	
+
+	
+function drawChart(data) {
+	
+	var group = svg.append("g");
+	
+	group.append("line")
+			.attr("class", "separation")
+			.attr("y1", x(startDate))
+			.attr("y2", x(endDate))
+			.attr("x1", 30)
+			.attr("x2", 30)
+			.attr("opacity", .6)
+			.attr("stroke", "black");
+	
+	group.selectAll('circle')
+		.data(data)
+		.enter()
+		.append('circle')
+		.attr('id', function(d){ 
+			return d.uid;
+		})
+		.attr("cx",30)
+		.attr("cy",function(d) {
+
+			var date = d.date_txt;
+			//var y = d.date_assassinat.slice(-4);
+			var y = x(format.parse(date));
+			if (d.uid==1) {
+				return y+10;
+				}
+			if (d.uid==9) {
+				return y+6;
+				}				
+			return y;
+		})
+		.attr("r",5)
+		.style("stroke","#fff")
+		.style("fill","#4c7d95")
+		.on('mouseover', tip.show)
+		.on('mouseout', tip.hide);
+		
+	group.selectAll('text')
+		.data(data)
+		.enter()
+		.append('text')
+		.attr("class","nomtrav")
+		.attr("x",50)
+		.attr("y",function(d) {
+			var date = d.date_txt;
+			var y = x(format.parse(date));
+			return y+4;
+		})
+		.style("fill","black")
+		.text(function(d) {
+			return d.nom;
+		});
+		
+		
+		if ((fixe)&&(window.innerWidth>=375))  {
+		tip.show(data[data.length-1], document.getElementById('13'));
+		}
+		if ((fixe)&&(window.innerWidth<375))  {
+		tip.show(data[data.length-1], document.getElementById('11'));
+		}
+	
+			
+}	
+//Appel à la Google sheet par 2 fonctions
+ function renderSpreadsheetData() {
+    Tabletop.init( { key: public_spreadsheet_url,
+                     callback: draw,
+                     simpleSheet: true } )
+}
+function draw(data, tabletop) {
+  // draw chart
+  drawChart(data);
+	}
+
+renderSpreadsheetData();
+ svg.call(tip);
+
--- ScrollPath/Episode2/script.js
+++ ScrollPath/Episode2/script.js
@@ -1,20 +1,12 @@
-//A completer a la main
-//Utilisation : Si on indique des pourcentages, il faut bien avoir donné un id unique a chaque p de l'html et la taille de l'array pourcent doit etre le nombre de paragraphe p.
 
-// Description pour Facebook (les og:type)
-	var sous_titre = "Au pays de la 389 - Péninsule du Labrador : la grande boucle";
-	var descrip =  "Sur la Basse-Côte-Nord, on peut rejoindre Blanc-Sablon en faisant un énorme arc de cercle par le Nord québécois et le Labrador terre-neuvien. Le Devoir a fait le long périple et bouclé la boucle de Baie-Comeau à Baie-Comeau, une distance de près de 3000 km.";
-	var lien= "";
+	var sous_titre = "Au pays de la 389 - Routiers de l’infini";
+	var descrip =  "Sur la Basse-Côte-Nord, on peut rejoindre Blanc-Sablon en faisant un énorme arc de cercle par le Nord québécois et le Labrador terre-neuvien. Le Devoir a fait le long périple et bouclé la boucle de Baie-Comeau à Baie-Comeau, une distance de près de 3000 km.";
+	var lien= "http://www.ledevoir.com/documents/special/16-07_route389_2eepisode/index.html";
 	
-	//Proportion du tracé pour chaque paragraphe.
-	var débutPourcent = 0;
-	var pourcent = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,4,0,13,7,8,10,0,0,0,0]; //exemple : [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,4,0,13,7,8,10,0,0,0,0];
-
+//Proportion du tracé pour chaque paragraphe.
+	var débutPourcent = 38;
+	var pourcent = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]; //exemple : [10,20,10,15,15,10,20,0];
 	
-	
-	
-	
-//Calcul somme pourcent
 var pourcentSomme = [débutPourcent];
 for(var i=1; i<pourcent.length; i++){
 		pourcentSomme.push(pourcentSomme[i-1]+pourcent[i]);
@@ -24,7 +16,6 @@ for(var i=1; i<pourcent.length; i++){
 var width = window.innerWidth;
 var height = window.innerHeight;
 
-// Pour adapter la navbar a l'ecran
 var divs = document.getElementsByTagName('img');
 
 for(var i=0; i<divs.length; i++){
@@ -42,12 +33,12 @@ document.getElementById('text').style.marginTop = $("#nav").height()+'px';
 document.getElementById('svg').style.marginTop = $("#nav").height()+'px';
 
 if ((0.3*width*580/250)>0.9*height) {
+	console.log('correction');
 	document.getElementById('svg').style.height = 0.9*height+'px';
 }
 else document.getElementById('svg').style.width = 0.3*width+'px';
 
 
-//Pour ouvrir la pop up de partage facebook ou twitter
 function openfb(url) {
 	window.open(url, 'sharer', 'top=0,left=0,toolbar=0,status=0,width=520,height=350');};
 
@@ -56,7 +47,6 @@ function openfb(url) {
 	document.getElementById("lienfb").href='javascript:openfb( "https://www.facebook.com/dialog/feed?app_id=256172254741882&link='+ window.location.href+'&title='+sous_titre+'&description='+descripP+'&redirect_uri=http://ledevoir.com&picture='+lien+'" )';
 	document.getElementById("lientw").href='javascript:openfb( "https://twitter.com/intent/tweet?url='+ window.location.href+'&text='+sous_titre+' @ledevoir&related=@ledevoir&counturl='+ window.location.href+'" )'
 
-//Fonction pour trouver a quel paragraphe nous sommes	
 function index(tab,val) {
 	var ind = 0;
 	
@@ -68,21 +58,17 @@ function index(tab,val) {
 	return ind;
 };
 	
-// Fonction d'initialisation du scroll
 $(document).ready(function() {
 	
-	//Récupération du path pour décalé le pointillé	
 	var path = d3.selectAll(".path");
 	var totalLength = path.node().getTotalLength();
 	path.attr("stroke-dasharray", totalLength)
 		.attr("stroke-dashoffset", totalLength+totalLength*débutPourcent/100);
 	
 	
-	//Fonction au scroll
 	$(window).scroll(function() {
 	var scroll = $(window).scrollTop();
 	
-	//Si pas de pourcent, affiche linéaire
 	if (pourcent.length==0) {
 		path.attr("stroke-dashoffset", totalLength + totalLength*débutPourcent/100 + ((scroll*(totalLength-totalLength*débutPourcent/100))/($("html").height()-height)));
 	}
@@ -118,4 +104,3 @@ $(document).ready(function() {
 	}
 	});
 });
-
